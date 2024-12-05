a7b0b6d36465114936ac86b8535a7742a5934942
Mahery Antonio <109660691+mahery76@users.noreply.github.com>
2024-12-05 18:03:38 +0300

    challenge: student with property

---
+++ Program16_Challenge1_Student_Properties/Program.cs
@@ -0,0 +1,35 @@
+﻿class Student // Student Class
+{
+
+  // Declare Fields Here
+  private string _name;
+  private string _rollNumber;
+  
+  // Define Properties here
+  public string Name {
+    set{this._name = value;}
+    get{return this._name;}
+  }
+
+  public string RollNumber{
+    set{this._rollNumber = value;}
+    get{return this._rollNumber;}
+  }
+  
+}
+
+class Program 
+{
+  public static void Main()
+  {
+    // Uncomment the below to test your implementation
+    Student student = new Student();
+    student.Name = "John";
+    student.RollNumber = "20";
+    System.Console.WriteLine(student.Name + " " + student.RollNumber);
+
+    // In an Encapsulated implementation the following should return an error
+    // student._name = "Mahery";
+    // student._rollNumber = "24";
+  }
+}
